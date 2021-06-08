000c74c029e515302d57114b2783677802ea4bdc
Philipp Jurasic <philipp.jurasic@gmx.de>
2021-06-08 22:48:23 +0200

    added pride easter egg

--- scripts/thoughts.js
+++ scripts/thoughts.js
@@ -113,6 +113,12 @@
     var now = new Date()
     return (now.getMonth() == date.month - 1 && ('day' in date ? now.getDate() == date.day : true))
   }
+  //pride month
+  if (isThisToday({ month: 6 }) || isThisToday({ month: 1 })) {
+    txt.push("Happy Pride Month")
+    for (pr of document.getElementsByClassName('profile'))
+      pr.classList.add('pride')
+  }
   //snow in december and january
   if (isThisToday({ month: 12 }) || isThisToday({ month: 1 })) {
     (function () { var a = document.getElementById("snow"), c = a.getContext("2d"), e = []; a.style.pointerEvents = "none"; a.style.position = "fixed"; a.style.top = 0; a.style.left = 0; a.style.width = "100vw"; a.style.height = "100vh"; a.height = a.offsetHeight; a.width = a.offsetWidth; window.onresize = function () { a.height = a.offsetHeight; a.width = a.offsetWidth }; var d = Math; setInterval(function () { c.clearRect(0, 0, a.width, a.height); c.beginPath(); var f = d.random(), g = .05 + .95 * f; flake = {}; flake.x = 1.5 * a.width * d.random() - .5 * a.width; flake.y = -9; flake.b = 2 * g * (d.random() / 2 + .5); flake.c = (4 + 2 * d.random()) * g; flake.a = d.pow(5 * f, 2) / 5; flake.update = function () { this.x += this.b; this.y += this.c; c.beginPath(); c.arc(this.x, this.y, this.a, 0, 2 * d.PI, !1); c.fillStyle = "#FFF"; c.fill() }; e.push(flake); for (b = 0; b < e.length; b++)e[b].y > a.height ? e.splice(b, 1) : e[b].update() }, 16) })();
