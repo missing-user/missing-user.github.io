06530825360e83f31b03e785aef9844acaf9ba3e
Philipp Jurasic <philipp.jurasic@gmx.de>
2021-10-28 21:54:20 +0200

    added quotes

--- scripts/thoughts.js
+++ scripts/thoughts.js
@@ -56,7 +56,7 @@
     '"I love RegEx!" - nobody ever',
     "Here's Johnny!",
     "You're tearing me apart, Lisa!",
-    "Anyway, how's your sex life?",
+    //"Anyway, how's your sex life?",
     "Was nothing real?",
     "HELP! I’m In a Nutshell!",
     "Oh behave!",
@@ -77,10 +77,14 @@
     "Houston, we have a problem.",
     "What was the password again?",
     "Have you tried turning it off and on again?",
-    "Weddings Are Basically Funerals With A Cake",
+    "Weddings are basically funerals with a cake",
     "Wubba Lubba Dub Dub!",
+    "Let's go. In and out. Twenty minute adventure.",
     "You can't fail if you don't even try",
-    "WHY ARE WE ALWAYS SCREAMING AT SQL?"
+    "WHY ARE WE ALWAYS SCREAMING AT SQL?",
+    "It’s dangerous to go alone, take this!",
+    "It's not a problem, it's a learning opportunity",
+    "I'm probably thinking about skiing right now"
   ];
 
   function typeWriter() {
@@ -135,13 +139,14 @@
     for (pr of document.getElementsByClassName('profile'))
       pr.classList.add('pride')
   }
-  //snow in december and january
+  //snow in december and january, add a quote
   if (isThisToday({ month: 12 }) || isThisToday({ month: 1 })) {
+    txt.push("See you on the slopes!", "Let's shred some pow",  "Wanna go skiing tomorrow?")
     (function () { var a = document.getElementById("snow"), c = a.getContext("2d"), e = []; a.style.pointerEvents = "none"; a.style.position = "fixed"; a.style.top = 0; a.style.left = 0; a.style.width = "100vw"; a.style.height = "100vh"; a.height = a.offsetHeight; a.width = a.offsetWidth; window.onresize = function () { a.height = a.offsetHeight; a.width = a.offsetWidth }; var d = Math; setInterval(function () { c.clearRect(0, 0, a.width, a.height); c.beginPath(); var f = d.random(), g = .05 + .95 * f; flake = {}; flake.x = 1.5 * a.width * d.random() - .5 * a.width; flake.y = -9; flake.b = 2 * g * (d.random() / 2 + .5); flake.c = (4 + 2 * d.random()) * g; flake.a = d.pow(5 * f, 2) / 5; flake.update = function () { this.x += this.b; this.y += this.c; c.beginPath(); c.arc(this.x, this.y, this.a, 0, 2 * d.PI, !1); c.fillStyle = "#FFF"; c.fill() }; e.push(flake); for (b = 0; b < e.length; b++)e[b].y > a.height ? e.splice(b, 1) : e[b].update() }, 16) })();
   }
   //christmas quotes
   if (isThisToday({ day: 24, month: 12 }) || isThisToday({ day: 25, month: 12 }))
-    txt.push("Ho, ho, ho...", "Merry Christmas!", "I hope you get lots of presents", "Happy holidays!", "Hey Google, play WHAM! Last Christmas")
+    txt.push("Ho, ho, ho...", "Merry Christmas!", "I hope you get lots of presents", "Happy holidays!", "Hey Google, play 'Last Christmas'")
   //birthday quotes
   if (isThisToday({ day: 11, month: 12 }))
     txt.push("It's my birthday!!!", "I woder what presents I'll get", "You're invited!", "Happy birthday to me")
